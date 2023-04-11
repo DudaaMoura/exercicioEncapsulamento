5f70bb4727d5b9d1534a24cd7dead10c25c8926e
DudaaMoura <mariamourapalmital@gmail.com>
2023-04-11 15:00:19 +0000

    conta

---
+++ Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+    public static void main(String[] args) {
+        ContaBancaria cb = new ContaBancaria();
+
+        cb.setNumeroConta(10000);
+        cb.setTitular("Duda");
+        cb.setSaldo(125);
+
+        System.out.println("Nome do titular: "+cb.getTitular());
+
+        System.out.println("Número da conta: "+cb.getNumeroConta());
+
+        System.out.println("Saldo atual: "+cb.getSaldo());
+
+       
+    }
+    
+}
